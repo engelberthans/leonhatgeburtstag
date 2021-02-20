c00bf7ac3339043f897f029c0eb200e9676dec10
engelberthans <79315699+engelberthans@users.noreply.github.com>
2021-02-20 19:43:02 +0100

    test1

---
+++ karte.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="karte" tilewidth="2184" tileheight="1325" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="2184" height="1325" source="../Bildschirmfoto 2021-02-20 um 07.28.11.png"/>
+ </tile>
+</tileset>
